ebccbaa8a3f97aa0f843526ba391fdcbbbb9dfbd
Taichi Souma <izewfktvy533zjmn@gmail.com>
2019-06-20 16:51:09 +0900

    Update emulator.h emulator.cpp
    I implement init, destroy, dumpMemory methods.

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -1,10 +1,16 @@
+#include <iostream>
+#include <cstring>
+
 #include "../include/emulator.h"
 
 
-int Emulator::init() {
+void Emulator::init(FILE* fp) {
+    memory = new uint8_t[MEMORY_SIZE_BYTE];
+    memset(memory, 0, sizeof(memory));
+    std::fread(memory, 1, MEMORY_SIZE_BYTE, fp);
 }
 
-
+/*
 int Emulator::fetch() {
 }
 
@@ -20,10 +26,11 @@ int Emulator::operand() {
 int Emulator::writeback() {
 }
 
-
-int Emulator::destory() {
+*/
+void Emulator::destroy() {
+    delete memory;
 }
-
+/*
 
 void Emulator::dump() {
 }
@@ -31,11 +38,22 @@ void Emulator::dump() {
 
 void Emulator::dumpRegisters() {
 }
-
+*/
 
 void Emulator::dumpMemory() {
+    for(int i=0; i<MEMORY_SIZE_BYTE/2; i+=2) {
+        if(!(i==0) && !(i % 16)) {
+            std::printf("\n");
+        }
+        if(!(i % 16)) {
+            std::printf("%07x ", i);
+        }
+        std::printf("%02x%02x ", memory[i+1], memory[i]);
+    }
+    std::printf("\n");
 }
 
-
+/*
 void Emulator::dumpOperand() {
 }
+*/
