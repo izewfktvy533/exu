ae835e70e75e24517ca1ecf21d430594b32ef368
Taichi Souma <izewfktvy533zjmn@gmail.com>
2019-06-20 15:19:57 +0900

    Add emulator.h emulator.c
    I implement framework of emulator.

---
+++ src/emulator.cpp
@@ -0,0 +1,41 @@
+#include "../include/emulator.h"
+
+
+int Emulator::init() {
+}
+
+
+int Emulator::fetch() {
+}
+
+
+int Emulator::decode() {
+}
+
+
+int Emulator::operand() {
+}
+
+
+int Emulator::writeback() {
+}
+
+
+int Emulator::destory() {
+}
+
+
+void Emulator::dump() {
+}
+
+
+void Emulator::dumpRegisters() {
+}
+
+
+void Emulator::dumpMemory() {
+}
+
+
+void Emulator::dumpOperand() {
+}
