f3d158c496ebd381c9ce90f7e14d215bea43f60b
Taichi Souma <izewfktvy533zjmn@gmail.com>
2019-06-20 15:19:57 +0900

    Add emulator.h emulator.c
    I implement framework of emulator.

---
+++ include/emulator.h
@@ -0,0 +1,48 @@
+#ifndef _EMULATOR_H_
+#define _EMULATOR_H_
+
+#include "fetcher.h"
+#include "decoder.h"
+#include "operander.h"
+#include "excuter.h"
+#include "writebacker.h"
+
+
+class Emulator {
+    public:    
+        int  init();
+        int  fetch();
+        int  decode();
+        int  operand();
+        int  execute();
+        int  writeback();
+        int  destory();
+        void dump();
+        void dumpRegisters();
+        void dumpMemory();
+        void dumpOpecode();
+
+    private:
+        enum Registers {
+            EAX,
+            ECX,
+            EDX,
+            EBX,
+            ESP,
+            EBP,
+            ESI,
+            EDI,
+            EIP,
+            REGISTERS_COUNT
+        };
+        std::uint32_t    registers[REGISTERS_COUNT];
+        std::uint8_t*    memory;
+        Fetcher     fetcher;
+        Decoder     decoder;
+        Operander   operander;
+        Executer    executer;
+        Writebacker writebacker;
+};
+
+
+#endif _EMULATOR_H_
